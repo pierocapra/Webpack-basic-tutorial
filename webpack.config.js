033ad8f013d61b3ab808f199339c0b72a383b319
Piero <pcapra@squiz.co.uk>
2022-02-11 11:10:00 +0000

    add publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,14 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, './dist')
+    path: path.resolve(__dirname, './dist'),
+    publicPath: 'dist/'
   },
   mode: 'none',
   module: {
     rules: [
       {
-        test: /\.(png|jpeg)$/,
+        test: /\.(png|jpg|jpeg)$/,
         type: 'asset/resource'
       }
     ]
