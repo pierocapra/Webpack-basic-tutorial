a174a12102b144f86569de36e206e70fe466269f
Piero <pcapra@squiz.co.uk>
2022-02-14 10:07:06 +0000

    Generate Html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,14 @@ const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, './dist'),
-    publicPath: 'dist/'
+    publicPath: ''
   },
   mode: 'none',
   module: {
@@ -48,6 +49,12 @@ module.exports = {
             plugins: [ '@babel/plugin-proposal-class-properties' ]
           }
         }
+      },
+      {
+        test: /\.hbs$/,
+        use: [
+          'handlebars-loader'
+        ]
       }
     ]
   },
@@ -62,5 +69,10 @@ module.exports = {
         path.join(process.cwd(), 'build/**/*')
       ]
     }),
+    new HtmlWebpackPlugin({
+      title: 'Hello World',
+      template: 'src/index.hbs',
+      description: 'Some descriptions',
+    })
   ]
 }
